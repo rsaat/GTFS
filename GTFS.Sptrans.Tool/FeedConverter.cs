f6e6267e6b45cbde6aa9825ccb9baa4977f90f01
rsaat <rsaat@usa.com>
2015-03-01 15:55:02 -0300

    Start to building a simple tool to add some GTFS data to SPTrans database.
    Fill approximate value of shape_dist_traveled field from stop_time using shape informations.

---
+++ GTFS.Sptrans.Tool/FeedConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using GTFS.DB;
+using GTFS.DB.SQLite;
+using GTFS.IO;
+
+namespace GTFS.Sptrans.Tool
+{
+    class FeedConverter
+    {
+        public static void ConvertFeedToSqliteDb(string sptransSqliteDbFile, string sptransFeedDirectory)
+        {
+            if (File.Exists(sptransSqliteDbFile))
+            {
+                File.Delete(sptransSqliteDbFile);
+            }
+
+            string sqliteConnectionString = string.Format(@"Data Source={0};Version=3;", sptransSqliteDbFile);
+            // get test db.
+            var db = CreateDB(sqliteConnectionString);
+
+            // build test feed.
+            var feed = CreateSptransFeedFromTextFiles(sptransFeedDirectory);
+
+            // add to db.
+            db.AddFeed(feed);
+
+            
+        }
+
+        private static IGTFSFeedDB CreateDB(string connectionString)
+        {
+            return new SQLiteGTFSFeedDB(connectionString);
+        }
+
+        private static IGTFSFeed CreateSptransFeedFromTextFiles(string sptransFeedDirectory)
+        {
+
+            // create the reader.
+            var reader = new GTFSReader<GTFSFeed>(false);
+
+            // execute the reader.
+            var feed = reader.Read(new GTFSDirectorySource(new DirectoryInfo(sptransFeedDirectory)));
+
+            return feed;
+        }
+    }
+}
